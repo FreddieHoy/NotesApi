b83627aeee39f799c6a905c8c27fa75e0a0bf7d4
Freddie Hoy <freddiehoy0@gmail.com>
2023-01-16 09:09:21 +0000

    update temp password

--- src/dbPool.ts
+++ src/dbPool.ts
@@ -3,11 +3,11 @@ import { Pool } from "pg";
 // should be hidden using .env
 // This is connecting to the DB created using the pgsql command line
 export const pool = new Pool({
-  user: process.env.DB_USER || "me",
+  user: process.env.DB_USER || "freddie",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "api",
-  password: process.env.DB_PASS || "password", // TODO ADD TO >.env
-  port: 5432,
+  password: process.env.DB_PASS || "freddielovescake", // TODO ADD TO >.env
+  port: 54321,
 });
 
 export const secret = process.env.JWT_SECRET || "dev_secret123";
